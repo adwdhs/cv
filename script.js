1dd29f1064d024cb42b76c3b801ffa9918c44092
Andrii Harkusha <andriy.garkusha111@gmail.com>
2026-04-19 18:35:44 +0200

    66741 zad5: formularz i walidacja

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+ 
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const firstName = document.getElementById("firstName").value.trim();
+        const lastName = document.getElementById("lastName").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const message = document.getElementById("message").value.trim();
+
+        if (!firstName || !lastName || !email || !message) {
+            alert("Wszystkie pola są wymagane!");
+            return;
+        }
+        if (/\d/.test(firstName) || /\d/.test(lastName)) {
+            alert("Imię i nazwisko nie mogą zawierać cyfr!");
+            return;
+        }
+        if (!email.includes("@") || !email.includes(".")) {
+            alert("Niepoprawny email!");
+            return;
+        }
+
+        alert("Wysłano pomyślnie!");
+        
+    });
+ 
+
+});
+
+
 function toggleTheme() {
     const body = document.body;
     body.classList.toggle("theme-red");
